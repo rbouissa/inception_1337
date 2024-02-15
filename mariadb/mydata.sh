0bc31c0c026b73289d1b4039d6f3fcf492a39ae8
rbouissa <rbouissa@student.1337.ma>
2024-02-16 00:06:12 +0100

    Dsdsds

---
+++ mariadb/mydata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CONFIG_FILE="/etc/mysql/mariadb.conf.d/50-server.cnf"
+sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' "$CONFIG_FILE"
+mysql_install_db
+service mariadb start
+# mysql_secure_installation << RR
+# Y
+# Y
+# Y
+# Y
+# Y
+# Y
+# RR
+
+#mkdir /run/mysqld
+echo "create database rr" | mysql
+echo "CREATE USER IF NOT EXISTS 'ss'@'localhost' IDENTIFIED BY '1234';" | mysql
+echo "GRANT ALL PRIVILEGES ON \`rr\`.* TO \`ss\`@'%' IDENTIFIED BY '1234';" | mysql
+echo "flush privileges;" | mysql
+service mariadb stop
+mariadbd
+
+# exec "$@"
