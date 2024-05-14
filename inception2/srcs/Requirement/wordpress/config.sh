65ac8551872186d3a42af25cb234bdceb648edf5
rbouissa <redabouissali422@gmail.com>
2024-05-14 23:16:53 +0100

    dsdsd

---
+++ inception2/srcs/Requirement/wordpress/config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sleep 10
+
+sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+# chmod 777 "/var/www/html/*"
+# mkdir -p /run/php
+wp core download --allow-root
+wp config create --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --allow-root
+wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINEMAIL --allow-root
+wp user create $USER $USEREMAIL --role=subscriber --user_pass=$USERPASS --allow-root
+chown -R www-data:www-data /var/www/html
+
+echo "wordpres run"
+php-fpm7.3 -F -R --nodaemonize
